6d3e12b57c47025842887195d9229c98d6ed418c
sivasuryan-net <sivasuryan.net@gmail.com>
2026-03-17 22:04:34 +0530

    Add comments for clarity in index.js

--- index.js
+++ index.js
@@ -6,18 +6,24 @@ const swaggerDocument = require('./swagger_output.json');
 const connectDB = require('./src/db/connection');
 dotenv.config();
 
+// Initialize Express app
 const port = process.env.PORT || 8000;
 const app = express();
 
+// Connect to the database
 connectDB();
 
+// Swagger UI setup
 app.use('/api-docs', swggerui.serve, swggerui.setup(swaggerDocument));
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Routes
 app.use('/api', require('./src/routes/index'));
 
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
